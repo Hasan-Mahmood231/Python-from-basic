5d3a52754513ccdf50aed25e622ec13792c3525a
Hasan-Mahmood231 <hk4145559@gmail.com>
2025-04-12 22:34:26 -0700

    checking enviroment

--- Bmi_calculator.py
+++ Bmi_calculator.py
@@ -1,6 +1,6 @@
 #---------------------BMI CALCULATORE-----------------------
 class Bmi_Calculator:
-    
+     
     weightInkgs = 79
     hightInmeter = 4
     
@@ -11,15 +11,13 @@ class Bmi_Calculator:
 person1 = Bmi_Calculator()
 person2 = Bmi_Calculator()
 #---------------person2 detail--------------
-person2.weightInkgs = 79
+person2.weightInkgs = 74
 person2.hightInmeter = 3
 
-
 #--------------printing-------------
-print(f"person 1 weigh is{person1.weightInkgs}")
-print(f"person 1 hight is{person1.hightInmeter}")
+print(f"person 1 weigh is :{person1.weightInkgs}")
+print(f"person 1 hight is :{person1.hightInmeter}")
 person1.cal_bmi()
-# print(f"The bmi of person 1 is {person1.cal_bmi()}")
 print("------------------------------------",end="")
 
 print(f"person 2 weight is{person1.weightInkgs}")
