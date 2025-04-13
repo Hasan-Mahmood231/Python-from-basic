671c10f45a2b3627e07c946021900f9cb767b1ba
Hasan-Mahmood231 <hk4145559@gmail.com>
2025-04-12 23:05:20 -0700

    add some new file

---
+++ Bmi_calculator.py
@@ -0,0 +1,28 @@
+#---------------------BMI CALCULATORE-----------------------
+class Bmi_Calculator:
+    
+    weightInkgs = 79
+    hightInmeter = 4
+    
+    def cal_bmi(self):
+        bm = (self.weightInkgs)/(self.hightInmeter)
+        print(f"bmi is {bm}")
+    
+person1 = Bmi_Calculator()
+person2 = Bmi_Calculator()
+#---------------person2 detail--------------
+person2.weightInkgs = 79
+person2.hightInmeter = 3
+
+
+#--------------printing-------------
+print(f"person 1 weigh is{person1.weightInkgs}")
+print(f"person 1 hight is{person1.hightInmeter}")
+person1.cal_bmi()
+# print(f"The bmi of person 1 is {person1.cal_bmi()}")
+print("------------------------------------",end="")
+
+print(f"person 2 weight is{person1.weightInkgs}")
+print(f"person 2 hight is{person1.hightInmeter}")
+person2.cal_bmi()
+# print(f"the bmi of person 2 is {person2.cal_bmi()}")
---
+++ class&obj.py
@@ -0,0 +1,18 @@
+class Customer:
+    name = "ajmal"
+    product= "max_food"
+    total_bill = 4300
+    
+customer1 = Customer()
+customer2 = Customer()
+customer3 = Customer()
+
+#----------------------CUSTOMER 2--------------------------
+customer2.name = "Tariq"
+customer2.product = "dry fruit"
+customer2.total_bill = 4400
+#----------------------CUSTOMER 3--------------------------
+customer3.name = "Ahmad"
+customer3.product = "max-food"
+customer3.total_bill = 98300
+#-----------------------------------------------------
